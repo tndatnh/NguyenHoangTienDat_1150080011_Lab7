8a6445709acfb3bc87f05701e342a73f96cb42d0
tndatnh <110185333+tndatnh@users.noreply.github.com>
2025-11-03 08:42:39 +0700

    Add files via upload

---
+++ NguyenHoangTienDat_1150080011_BTtuan8/NguyenHoangTienDat_1150080011_BTtuan8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NguyenHoangTienDat_1150080011_BTtuan8
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMatHang());
+        }
+    }
+}
